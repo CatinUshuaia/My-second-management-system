d08bea1e784a7261bb73c6c7cde9941d0a5954ec
CatinUshuaia <140497913+CatinUshuaia@users.noreply.github.com>
2023-08-03 17:50:57 +0800

    Add files via upload
    up to 34-- ready to login

---
+++ src/views/Formsubmit.tsx
@@ -0,0 +1,10 @@
+﻿const View=()=> {
+    return (
+        <div className="home" style={{ fontSize:30,textAlign: 'center', padding: 0, lineHeight: '48px',color:'blue' }} >
+            <p>这是Formsubmit页面</p>
+            <p>请在这里提交你的表格</p>
+        </div>
+    )
+}
+
+export default View
---
+++ src/views/Password.tsx
@@ -0,0 +1,8 @@
+
+export default function View() {
+    return (
+        <div className="user">
+            <p>Password</p>
+        </div>
+    )
+}
