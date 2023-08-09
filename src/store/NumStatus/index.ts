bdb9cd93ccb8ebb190f6ce360211743a6f5730dc
CatinUshuaia <140497913+CatinUshuaia@users.noreply.github.com>
2023-08-09 14:53:45 +0800

    Add files via upload

---
+++ src/store/NumStatus/index.ts
@@ -0,0 +1,39 @@
+﻿const store= {
+    state: {
+        num: 30
+    },
+    actions: {//只放同步的方法
+
+        add1(newState: { num: number }, action: { type: string }) {
+                newState.num++
+        },
+
+        add2(newState:{ num: number }, action: { type: string ,val:number}) {
+                newState.num += action.val
+        },
+    },
+
+
+    //优化redux-thunk的异步写法（模仿Vuex的写法）
+    asyncActions: {//只放异步的方法
+    asyncAdd1(dispatch: Function){
+    setTimeout(() => {
+        dispatch({ type: "add1" })
+    }, 1000)
+    }
+},
+    actionNames: {}
+}
+
+
+
+let actionNames = {};
+
+for (let key in store.actions) {
+    actionNames[key] = key;
+}
+
+store.actionNames = actionNames;
+
+
+export default store
---
+++ src/views/Formsubmit.tsx
@@ -0,0 +1,36 @@
+﻿import { useDispatch, useSelector } from "react-redux"
+import numStatus from "@/store/NumStatus"
+
+const View = () => {
+    const dispatch = useDispatch()
+    //通过useSelector获取仓库数据
+    const {num} = useSelector((state:RootState) => ({
+        num:state.handleNum.num
+    }))
+
+    //修改仓库数据
+    const changeNum = () => {
+        dispatch(numStatus.asyncActions.asyncAdd1)
+
+    }
+
+    const changeArr = () => {
+        dispatch({ type: "sarrpush", val: 10 })
+    }
+
+    //对sarr的操作
+
+    return (
+        <div className="home" style={{ fontSize:30,textAlign: 'center', padding: 0, lineHeight: '48px',color:'blue' }} >
+            <p>这是Formsubmit页面</p>
+            <p>请在这里提交你的表格</p>
+            <p>{num}</p>
+            <button onClick={changeNum}>异步操作按钮</button>
+            <div>
+                <button onClick={changeArr}>同步操作按钮</button>
+            </div>
+        </div>
+    )
+}
+
+export default View
